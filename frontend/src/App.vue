502c417c7026ebf7c6c6be5fbcaeee0a659495bd
Davi Martins Lage <davimartinslage2005@gmail.com>
2025-04-08 16:35:28 -0300

    feat: Adicionado homepage utilizando tailwind css e shadcn

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <div class="min-h-screen bg-background">
+    <nav class="bg-gray-800 text-white p-4 shadow-xl rounded-b-sm">
+      <div class="container mx-auto flex justify-between items-center">
+        <RouterLink to="/" class="text-lg font-bold">Home Page</RouterLink>
+      </div>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,61 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+</script>
 
 <template>
-  <main></main>
+  <main class="p-6">
+    <h1 class="text-4xl font-extrabold mb-8 text-center text-gray-800">Cidadão de Olho</h1>
+    <div class="flex flex-col md:flex-row p-4 gap-4">
+      <RouterLink to="/deputados" class="md:w-1/4">
+        <Card class="hover:scale-105 transition-transform duration-300 shadow-lg h-full">
+          <CardHeader>
+            <CardTitle>Deputados</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Listagem de todos os deputados</p>
+          </CardContent>
+        </Card>
+      </RouterLink>
+
+      <RouterLink to="/deputados" class="md:w-1/4">
+        <Card class="hover:scale-105 transition-transform duration-300 shadow-lg h-full">
+          <CardHeader>
+            <CardTitle>Verbas Indenizatórias</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Listar os deputados com maiores com verbas indenizatórias por mês</p>
+          </CardContent>
+        </Card>
+      </RouterLink>
+
+      <RouterLink to="/deputados" class="md:w-1/4">
+        <Card class="hover:scale-105 transition-transform duration-300 shadow-lg h-full">
+          <CardHeader>
+            <CardTitle>Verbas Indenizatórias - ANO</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Listar os deputados com maiores com verbas indenizatórias no ano</p>
+          </CardContent>
+        </Card>
+      </RouterLink>
+
+      <RouterLink to="/deputados" class="md:w-1/4">
+        <Card class="hover:scale-105 transition-transform duration-300 shadow-lg h-full">
+          <CardHeader>
+            <CardTitle>TOP 5 Redes sociais</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Exibir as 5 redes sociais mais utilizadas pelos deputados</p>
+          </CardContent>
+        </Card>
+      </RouterLink>
+    </div>
+  </main>
 </template>
