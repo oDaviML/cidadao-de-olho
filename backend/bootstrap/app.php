683080685f7f8529815e99d9a1f4fa653dc019b1
Davi Martins Lage <davimartinslage2005@gmail.com>
2025-04-09 13:17:42 -0300

    fix: adiciona tratamento de exceção para rotas não encontradas

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,17 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $ex) {
+            return response()->json([
+                'message' => 'Rota não encontrada.',
+                'status' => 'error',
+                'code' => 404,
+            ], 404);
+        });
     })->create();
